97c8869a0a8fa1595c8174e0d0dcd16c24cf8fca
John-Anthony Thevos <jgt@JohnAnthonysAir.attlocal.net>
2022-05-27 17:14:29 -0400

    changes to render yml

--- mysite/settings.py
+++ mysite/settings.py
@@ -149,8 +149,8 @@ if not DEBUG:
     AWS_S3_REGION_NAME = 'nyc3'
     AWS_S3_ENDPOINT_URL = 'https://jonathangreenstudios.nyc3.digitaloceanspaces.com'
 
-    AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
-    AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_STORAGE_BUCKET_NAME = 'images'
 
 
@@ -175,10 +175,10 @@ if not DEBUG:
     DATABASES += {
         'default': {
             'ENGINE': 'django.db.backends.postgresql',
-            'NAME': config('POSTGRES_DB_NAME'),
-            'USER': config('POSTGRES_USER'),
-            'PASSWORD': config('POSTGRES_PW'),
-            'HOST': config('POSTGRES_HOST'),
+            'NAME': os.environ.get('POSTGRES_DB_NAME'),
+            'USER': os.environ.get('POSTGRES_USER'),
+            'PASSWORD': os.environ.get('POSTGRES_PW'),
+            'HOST': os.environ.get('POSTGRES_HOST'),
             'PORT': '5432',
         }
     }
